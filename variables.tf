517b9b593c574aba54fe2df63082cb2fad474fe4
Marcelo Costa <marcelotpcosta@outlook.com>
2023-10-09 13:20:22 +0100

    first commit

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "project_id" {
+  type        = string
+  description = "Your project id"
+}
+
+variable "region" {
+  type        = string
+  description = "Region where to deploy VPN"
+}
+
+variable "zone" {
+  type        = string
+  description = "Zone where to deploy VPN"
+}
+
+variable "credentials_path" {
+  type = string
+  description = "Path to service account file"
+}
+
+variable "ssh_user" {
+  type        = string
+  description = "Username for ssh key"
+}
+
+variable "ssh_pub_key_path" {
+  type = string
+  description = "Path to public ssh key"
+}
+
+variable "server_port" {
+  type = string
+  description = "Server port through which VPN clients will be connected"
+}
